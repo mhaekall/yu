e0505ae6b9b70ce5277ce2eb9d61e2c19561f5cf
mhaekall <jonyoblak@gmail.com>
2025-04-25 09:56:22 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/layout/Navbar'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
